9291ca8bbf6c6733a8015f4b78e629e5a189ea0b
Shoirhi <dibella.flqxl@simplelogin.com>
2024-02-08 18:52:55 +0900

    PWAを追加

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,10 @@ import "./globals.css";
 
 export const runtime = 'edge';
 
+export const metadata: Metadata = {
+  title: "買い物",
+}
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -10,6 +14,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icon.png"></link>
+        <meta name="theme-color" content="#b8e986" />
+      </head>
       <body className="flex flex-col min-h-screen">
         <main className="flex-1">
           {children}
