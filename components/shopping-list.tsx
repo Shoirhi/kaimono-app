82410c10f2a25686694b1166aaa5fa639a44173a
HiroshiJorrvaskr <hiroshikamikawa@proton.me>
2024-02-16 00:00:04 +0900

    レイアウトを修正

--- components/shopping-list.tsx
+++ components/shopping-list.tsx
@@ -141,129 +141,135 @@ export default function ShoppingList() {
 
   return (
     <>
-      <div className="container py-3 border-b sticky top-0 bg-neutral-50 shadow z-10">
-        <div className="grid grid-cols-2 gap-x-4">
-          <div>
-            <Dialog>
-              <DialogTrigger asChild>
-                <Button variant="ghost" type="button">
-                  リセット
-                </Button>
-              </DialogTrigger>
-              <DialogContent className="sm:max-w-[425px]">
-                <DialogHeader>
-                  <DialogTitle>リセット</DialogTitle>
-                </DialogHeader>
-                <div className="py-4">
-                  <p className="text-center">
-                    リセットしますか？
-                    <br />
-                    この操作は元には戻せません。
-                  </p>
-                </div>
-                <DialogFooter>
-                  <DialogClose asChild>
-                    <Button
-                      variant="destructive"
-                      type="button"
-                      onClick={resetShoppingItems}
-                    >
-                      リセットする
-                    </Button>
-                  </DialogClose>
-                </DialogFooter>
-              </DialogContent>
-            </Dialog>
+      <div className="py-3 border-b sticky top-0 bg-neutral-50 shadow z-10">
+        <div className="container">
+          <div className="grid grid-cols-2 gap-x-4">
+            <div>
+              <Dialog>
+                <DialogTrigger asChild>
+                  <Button variant="ghost" type="button">
+                    リセット
+                  </Button>
+                </DialogTrigger>
+                <DialogContent className="sm:max-w-[425px]">
+                  <DialogHeader>
+                    <DialogTitle>リセット</DialogTitle>
+                  </DialogHeader>
+                  <div className="py-4">
+                    <p className="text-center">
+                      リセットしますか？
+                      <br />
+                      この操作は元には戻せません。
+                    </p>
+                  </div>
+                  <DialogFooter>
+                    <DialogClose asChild>
+                      <Button
+                        variant="destructive"
+                        type="button"
+                        onClick={resetShoppingItems}
+                      >
+                        リセットする
+                      </Button>
+                    </DialogClose>
+                  </DialogFooter>
+                </DialogContent>
+              </Dialog>
+            </div>
+            <div className="text-right">
+              ￥<span className="text-3xl font-bold pl-1">{totalPrice}</span>
+            </div>
+            <div></div>
           </div>
-          <div className="text-right">
-            ￥<span className="text-3xl font-bold pl-1">{totalPrice}</span>
-          </div>
-          <div></div>
         </div>
       </div>
       <div className="pt-5 pb-40">
-        {shoppingItems.length ? (
-          <Table>
-            <TableBody>
-              {shoppingItems.map((item, index) => (
-                <TableRow
-                  key={index}
-                  className={clsx(!item.active && "opacity-30")}
-                >
-                  <TableCell className="py-2">{item.name}</TableCell>
-                  <TableCell className="py-2">
-                    <div className="flex justify-end items-center gap-x-1">
-                      <div className="text-right">
-                        ￥
-                        <span className="text-xl font-bold pl-1">
-                          {item.price}
-                        </span>
+        <div className="container px-0">
+          {shoppingItems.length ? (
+            <Table>
+              <TableBody>
+                {shoppingItems.map((item, index) => (
+                  <TableRow
+                    key={index}
+                    className={clsx(!item.active && "opacity-30")}
+                  >
+                    <TableCell className="py-2">{item.name}</TableCell>
+                    <TableCell className="py-2">
+                      <div className="flex justify-end items-center gap-x-1">
+                        <div className="text-right">
+                          ￥
+                          <span className="text-xl font-bold pl-1">
+                            {item.price}
+                          </span>
+                        </div>
+                        <Button
+                          onClick={() => triggerActivation(index)}
+                          variant="ghost"
+                          size="icon"
+                        >
+                          {item.active ? (
+                            <Eye className="w-3 h-3" />
+                          ) : (
+                            <EyeOff className="w-3 h-3" />
+                          )}
+                        </Button>
                       </div>
-                      <Button
-                        onClick={() => triggerActivation(index)}
-                        variant="ghost"
-                        size="icon"
-                      >
-                        {item.active ? (
-                          <Eye className="w-3 h-3" />
-                        ) : (
-                          <EyeOff className="w-3 h-3" />
-                        )}
-                      </Button>
-                    </div>
-                  </TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        ) : (
-          <p className="text-center">アイテムを追加してください。</p>
-        )}
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          ) : (
+            <p className="text-center">アイテムを追加してください。</p>
+          )}
+        </div>
       </div>
-      <div className="fixed left-0 bottom-0 right-0 container py-3 border-t bg-neutral-50 shadow">
-        <div className="flex gap-2">
-          <div className="flex-1">
-            <div className="flex flex-col gap-2">
-              <Input
-                className="text"
-                placeholder="商品名"
-                type="text"
-                ref={nameInputRef}
-                value={newShoppingItemName}
-                onChange={(e) => onItemNameChange(e.target.value)}
-                onKeyDown={(e) => e.key === "Enter" && addShoppingItem()}
-              />
-              <div className="flex justify-end items-center gap-x-2">
-                <div className="font-bold text">￥</div>
-                <div className="max-w-28">
-                  <Input
-                    className="text text-right"
-                    placeholder="価格"
-                    type="tel"
-                    value={newShoppingItemPrice}
-                    onChange={(e) => {
-                      onItemPriceChange(e.target.value);
-                    }}
-                    onKeyDown={(e) => e.key === "Enter" && addShoppingItem()}
-                  />
+      <div className="fixed left-0 bottom-0 right-0 py-3 border-t bg-neutral-50 shadow">
+        <div className="container">
+          <div className="flex gap-2">
+            <div className="flex-1">
+              <div className="flex flex-col gap-2">
+                <Input
+                  className="text"
+                  placeholder="商品名"
+                  type="text"
+                  ref={nameInputRef}
+                  value={newShoppingItemName}
+                  onChange={(e) => onItemNameChange(e.target.value)}
+                  onKeyDown={(e) => e.key === "Enter" && addShoppingItem()}
+                />
+                <div className="flex justify-end items-center gap-x-2">
+                  <div className="font-bold text">￥</div>
+                  <div className="max-w-28">
+                    <Input
+                      className="text text-right"
+                      placeholder="価格"
+                      type="tel"
+                      value={newShoppingItemPrice}
+                      onChange={(e) => {
+                        onItemPriceChange(e.target.value);
+                      }}
+                      onKeyDown={(e) => e.key === "Enter" && addShoppingItem()}
+                    />
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div>
-            {availableButton ? (
-              <Button
-                className="h-full"
-                type="button"
-                onClick={addShoppingItem}
-              >
-                追加する
-              </Button>
-            ) : (
-              <Button className="h-full" type="button" disabled>
-                追加する
-              </Button>
-            )}
+            <div>
+              {availableButton ? (
+                <Button
+                  className="h-full"
+                  type="button"
+                  onClick={addShoppingItem}
+                >
+                  追加する
+                </Button>
+              ) : (
+                <Button className="h-full" type="button" disabled>
+                  追加する
+                </Button>
+              )}
+            </div>
           </div>
         </div>
       </div>
